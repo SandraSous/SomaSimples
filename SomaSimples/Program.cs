c5365a5eaeb244ac4ae6a3a14e4d57ae8a716cfe
SandraSous <sandra.sousarosa1985@gmail.com>
2024-04-15 05:31:07 -0300

    Adicionar arquivos de projeto.

---
+++ SomaSimples/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Globalization;
+
+namespace SomaSimples {
+
+
+    class Program { 
+    
+
+        static void Main(string[] args) { 
+
+            int A, B, C;
+
+            A = int.Parse(Console.ReadLine());
+            B = int.Parse(Console.ReadLine());
+
+            C = A + B;
+
+            Console.WriteLine("SOMA = " + C );
+        
+
+
+
+        }
+
+    }
+}
